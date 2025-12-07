730afac5ee114d947e08773e1c6dedfc4e01043a
falanfila <138436030+falanfila@users.noreply.github.com>
2025-12-07 21:06:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+var x = 0
+console.log("Hello world!")
+const alan = document.getElementById("mesajAlani");
+alan.textContent = "Merhaba Dünya! JavaScript ile bu yazıyı ekledim.";
+
+function p() {
+    document.getElementById("demo").innerHTML
+    =
+    x = 0;
+}
+
+function d() {
+    document.getElementById("demo").innerHTML
+    =
+    x++;
+}
+
+if (x = 50) {
+    window.alert("Cat will explode! Make it vomit!")
+}
+
+if (x >= 60) {
+    window.alert("Cat exploded.")
+}
